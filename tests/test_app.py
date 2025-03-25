9ae8347b0a4de8e975d2091b381c5d905e67285e
yernar <yernar.baiginzheyev@gmail.com>
2025-03-26 03:05:34 +0500

    test

--- tests/test_app.py
+++
@@ -1,8 +0,0 @@
-from app import app
-
-def test_index():
-    test_client = app.test_client()
-
-    response = test_client.get('/')
-    assert response.status_code == 200
-    assert b"Hello, CI/CD!" in response.data
